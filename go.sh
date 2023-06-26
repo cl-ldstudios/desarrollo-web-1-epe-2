f9bcc214bda84aa98bf329fa56f321b8181f52d2
Rolando Duarte <rduarte@rduarte-ixc-lnx.ixcomercio.com>
2023-06-25 23:09:31 -0400

    Ajustes

--- go.sh
+++
@@ -1,7 +0,0 @@
-sh kill.sh
-node index.js
-
-#APP_NAME_DOCKER=$(basename "$PWD")
-#sudo docker container rm -f $APP_NAME_DOCKER
-#sudo docker build -t $APP_NAME_DOCKER .
-#sudo docker run -p 3000:3000 -e ENV='test'  --name $APP_NAME_DOCKER $APP_NAME_DOCKER
